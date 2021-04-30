ab7c58c8a8822dc3c33052645d79ed25b79db1fe
sarahelshabrawy <hylrcs@gmail.com>
2021-04-30 07:48:43 +0200

    Semantics test

--- src/test/java/TinyLanguageTest.java
+++ src/test/java/TinyLanguageTest.java
@@ -1,24 +1,38 @@
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import tiny.ParseException;
-import tiny.TinyLanguage;
+import tiny.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.io.StringReader;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TinyLanguageTest {
     String code;
+    ExecuteVisitor v = new ExecuteVisitor();
+    HashMap<String, Integer> map = new HashMap<>();
 
-    @After
-    public void Reset(){
-        //reset static counters
-        TinyLanguage.Reset();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
 
+    @After
+    public void restoreStreams() {
+        //reset static counters
+        System.setOut(originalOut);
+        TinyLanguage.Reset();
+    }
+    //first some tests for parser
     @Test
     public void AEXP1() throws ParseException {
-        code = "(8000+565)";
+        code = "(8000+abcdef)";
         TinyLanguage.ReInit(new StringReader(code));
         assertEquals(TinyLanguage.AEXP(),3);
 
@@ -27,19 +41,19 @@ public class TinyLanguageTest {
     @Test
     public void AEXP2() throws ParseException {
 
-        code = "f:=(((7+a)+(b-8))+565)";
+        code = "((7+a)+(b-8))+565";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Prog(),1);
+        assertEquals(TinyLanguage.AEXP(),7);
 
     }
-
-    @Test
-    public void AEXP3() throws ParseException {
-        code = "(abcdef-123456789)";
-        TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.AEXP(),3);
-
-    }
+//
+//    @Test
+//    public void AEXP3() throws ParseException {
+//        code = "(abcdef-123456789)";
+//        TinyLanguage.ReInit(new StringReader(code));
+//        assertEquals(TinyLanguage.AEXP(),3);
+//
+//    }
 
 
     @Test
@@ -61,35 +75,45 @@ public class TinyLanguageTest {
     public void commandAssignment() throws ParseException {
         code = "a:=8;b:=(90+75)";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Prog(),2);
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+        assertEquals(currentState(new String[]{"a","b"},new int[]{8,165}), outContent.toString());
     }
 
     @Test
     public void testWhile() throws ParseException {
-        code ="while tt do a:=(a-1)";
+        code ="f:=5;while (!(f==10)^tt) do f:=(f+1)";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Prog(),2);
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+        assertEquals(currentState(new String[]{"f"},new int[]{10}), outContent.toString());
     }
     @Test
     public void testIF() throws ParseException {
-        code ="if tt then c:=0 else skip";
+        code ="b:=8;a:=8;if !(b==8) then while !(a==0) do a:=(a-1) else if(a == b) then a:=9 else skip";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Com(),3);
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+        assertEquals(currentState(new String[]{"a","b"},new int[]{9,8}), outContent.toString());
 
     }
 
     @Test
     public void whileIf() throws ParseException {
-        code ="if !(b==8) then while !ff do a:=(a-1) else if(a == b) then a:=9 else skip";
+        code ="b:=5;a:=8;if !(b==8) then while !(a==0) do a:=(a-1) else if(a == b) then a:=9 else skip";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Prog(),6);
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+        assertEquals(currentState(new String[]{"a","b"},new int[]{0,5}), outContent.toString());
     }
 
     @Test
     public void commands() throws ParseException {
         code ="a:=4; b:=6; c:=1; if ((a + 5) == (b + 6)) then c:=0 else skip";
         TinyLanguage.ReInit(new StringReader(code));
-        assertEquals(TinyLanguage.Prog(),6);
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+        assertEquals(currentState(new String[]{"a","b","c"},new int[]{4,6,1}), outContent.toString());
     }
 
     @Test(expected = ParseException.class)
@@ -97,7 +121,6 @@ public class TinyLanguageTest {
         code ="if (a == 1) then f:=d;if !(a == 1) then f:=99";
         TinyLanguage.ReInit(new StringReader(code));
         TinyLanguage.Prog();
-
     }
 
     @Test(expected = ParseException.class)
@@ -120,4 +143,20 @@ public class TinyLanguageTest {
         TinyLanguage.ReInit(new StringReader(code));
         TinyLanguage.Prog();
     }
+
+    @Test(expected = SemanticException.class)
+    public void undefinedVariable() throws ParseException {
+        code ="while tt do a:=(a-1)";
+        TinyLanguage.ReInit(new StringReader(code));
+        SimpleNode e = TinyLanguage.Prog();
+        e.jjtAccept(v,map);
+    }
+
+  static String currentState(String[] variables, int[] values){
+        StringBuilder currentState = new StringBuilder("Current State\r\n");
+      for (int i = 0; i < values.length; i++) {
+          currentState.append(variables[i]).append(": ").append(values[i]).append("\r\n");
+      }
+      return currentState.toString();
+  }
 }
